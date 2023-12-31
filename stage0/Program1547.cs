7ac36c62f669c8fa1ab2b87cc99826216349f5f7
Tamar Israeli <tamisrae@g.jct.ac.il>
2023-12-31 17:29:01 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program1547.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Targil10
+{
+    partial class program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1547();
+            Welcome2894();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2894();
+        private static void Welcome1547()
+        {
+            Console.Write("Enter your name: ");
+            string name = System.Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
