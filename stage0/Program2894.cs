809533c2697f46c2e7c3283658155dbe31eb470e
איטי חיימסון <ittichaimson@gmail.com>
2023-12-31 18:50:36 +0200

    Initial Code + Welcome function

---
+++ stage0/Program2894.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome2894()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
