61a93970fb25916379e4db92fa95cb70d580e3b1
איטי חיימסון <ittichaimson@gmail.com>
2024-02-05 00:55:48 +0200

    stage 4- 9,10,13 little

--- BL/BlApi/Factory.cs
+++ BL/BlApi/Factory.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BlApi;
 
-namespace BlTest;
-
-partial class Program
+namespace BlApi;
+public static class Factory
 {
-    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+    public static IBl Get() => new BlImplementation.Bl(); 
 }
---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlImplementation;
+using BlApi;
+internal class Bl : IBl
+{
+    public IWorker Worker => new WorkerImplementation();
+
+
+    public ITask Task => new TaskImplementation();
+}
